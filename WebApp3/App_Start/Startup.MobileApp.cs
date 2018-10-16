43b12e389988378ec049ec659a15c0a9260f75aa
Marcin Joniec <Marcin_Joniec@epam.com>
2018-10-16 19:18:52 +0200

    Add Swagger to Web API

--- WebApp3/App_Start/Startup.MobileApp.cs
+++ WebApp3/App_Start/Startup.MobileApp.cs
@@ -3,6 +3,9 @@ using Microsoft.Azure.Mobile.Server.Config;
 using Owin;
 using LightInject;
 using System.Web.Http.Cors;
+using WebApp3.Swagger;
+using System;
+using Swashbuckle.Application;
 
 namespace WebAppMobile
 {
@@ -10,6 +13,15 @@ namespace WebAppMobile
     {
         public static void ConfigureMobileApp(IAppBuilder app)
         {
+            // Swagger
+            var zumoApiHeader = new SwaggerHeaderParameters
+            {
+                Description = "The default header for app services defining their version",
+                Key = "ZUMO-API-VERSION",
+                Name = "ZUMO-API-VERSION",
+                DefaultValue = "2.0.0"
+            };
+
             var httpConfiguration = new HttpConfiguration();
 
             httpConfiguration.EnableCors(new EnableCorsAttribute("*", "*", "*"));
@@ -17,6 +29,15 @@ namespace WebAppMobile
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional });
+
+            //var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            httpConfiguration.EnableSwagger(x =>
+            {
+                x.SingleApiVersion("v1", "ToDoSwagger");
+                zumoApiHeader.Apply(x);
+                //authHeader.Apply(x);
+                //x.IncludeXmlComments(basePath + "\\bin\\ToDoSwagger.XML");
+            }).EnableSwaggerUi();
 
             var container = new ServiceContainer();
 
---
+++ WebApp3/Swagger/SwaggerHeaderParameters.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http.Description;
+using Swashbuckle.Application;
+using Swashbuckle.Swagger;
+
+namespace WebApp3.Swagger
+{
+    public class SwaggerHeaderParameters : IOperationFilter
+    {
+        public string Description { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string DefaultValue { get; set; }
+
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            operation.parameters = operation.parameters ?? new List<Parameter>();
+            operation.parameters.Add(new Parameter
+            {
+                name = Name,
+                description = Description,
+                @in = "header",
+                required = true,
+                type = "string",
+                @default = DefaultValue
+            });
+        }
+
+        public void Apply(SwaggerDocsConfig c)
+        {
+            c.ApiKey(Key).Name(Name).Description(Description).In("header");
+            c.OperationFilter(() => this);
+        }
+    }
+}
