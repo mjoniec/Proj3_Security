28c4858c091bdc19b68c087ff2d4832008320394
mjoniec <mart1n.joniec@gmail.com>
2019-02-17 18:53:26 +0100

    Move mqtt lib to main solution

---
+++ MQTTClientLib/MQTTClientLib.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
+
+namespace MQTTClientLib
+{
+    public class MQTTClientLib
+    {
+        private IMqttClient _client = new MqttFactory().CreateMqttClient();
+        private List<string> _messages = new List<string>();
+
+        public MQTTClientLib()
+        {
+            _client.ApplicationMessageReceived += (s, e) =>
+            {
+                _messages.Add(Encoding.UTF8.GetString(e.ApplicationMessage.Payload) + " | " + e.ApplicationMessage.Topic);
+            };
+        }
+
+        public async Task<MqttClientConnectResult> Connect(string ip, int port)
+        {
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(ip, port)
+                .Build();
+
+            return await _client.ConnectAsync(options);
+        }
+
+        public async void Subscribe(string topic)
+        {
+            await _client.SubscribeAsync(new TopicFilterBuilder().WithTopic(topic).Build());
+        }
+
+        public async void Publish(string topic, string message)
+        {
+            var mqttApplicationMessageBuilder = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(message)
+                .WithExactlyOnceQoS()
+                .WithRetainFlag()
+                .Build();
+
+            await _client.PublishAsync(mqttApplicationMessageBuilder);
+        }
+
+        public string GetAllMessages()
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var m in _messages)
+            {
+                stringBuilder.AppendLine(m + " | ");
+                stringBuilder.AppendLine();
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
