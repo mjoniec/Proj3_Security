d5c2db4dcb38b39ec70e42b0ed2a81614f9f34db
mjoniec <mart1n.joniec@gmail.com>
2019-02-24 21:13:12 +0100

    Add service for gold prices external api client

---
+++ ExternalGoldDataApiClient.Service/ExternalGoldDataApiClientService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ExternalGoldDataApiClient.Service
+{
+    public class ExternalGoldDataApiClientService : IHostedService, IDisposable
+    {
+        private readonly ILogger _logger;
+        private readonly IOptions<ExternalGoldDataApiClientConfig> _config;
+
+        public ExternalGoldDataApiClientService(ILogger<ExternalGoldDataApiClientConfig> logger, IOptions<ExternalGoldDataApiClientConfig> config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting external gold data api client service: " + _config.Value.Name);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping external gold data api client service.");
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _logger.LogInformation("Disposing....");
+        }
+    }
+
+    public class ExternalGoldDataApiClientConfig
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ExternalGoldDataApiClient.Service/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ExternalGoldDataApiClient.Service
+{
+    class Program
+    {
+        //invoke with cmd
+        //dotnet run --ExternalGoldDataApiClient:Name="Gold data service"
+
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+
+                    //section name the same as in invoke command
+                    //TODO - move to config
+                    services.Configure<ExternalGoldDataApiClientConfig>(hostContext.Configuration.GetSection("ExternalGoldDataApiClient"));
+                    services.AddSingleton<IHostedService, ExternalGoldDataApiClientService>();
+                })
+                .ConfigureLogging((hostingContext, logging) => {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                });
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
