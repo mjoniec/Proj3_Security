1307fa5c500e14b8958532d68caa9f0cebacda88
mjoniec <mart1n.joniec@gmail.com>
2019-03-07 19:40:27 +0100

    create gold core api service, remove old test code

--- Gold.Service/Controllers/GoldController.cs
+++ Gold.Service/Controllers/GoldController.cs
@@ -2,19 +2,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Mqtt.Client;
 
-namespace MqttTestClient1.Controllers
+namespace Gold.Service.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class GoldController : ControllerBase
     {
         private readonly MqttDualTopicClient _mqttDoubleChannelClientAsync;
         private static string ResponseMessage = "x";
 
-        public ValuesController()
+        public GoldController()
         {
             _mqttDoubleChannelClientAsync = new MqttDualTopicClient(
                 "localhost", 1883, "ResponseMqttTopic", "RequestMqttTopic", ResponseReceivedHandler);
+        }
+
+        // GET: api/Gold
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { ResponseMessage };
+        }
+
+        // POST: api/Gold
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _mqttDoubleChannelClientAsync.Send(value);
         }
 
         //Move this to service and use DI
@@ -23,22 +37,6 @@ namespace MqttTestClient1.Controllers
             ResponseMessage = message;
 
             return message;
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-            _mqttDoubleChannelClientAsync.Send(value);
-
-            //return response;
-        }
-
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { ResponseMessage };
         }
     }
 }
