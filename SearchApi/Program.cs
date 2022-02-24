c38eab99216d16aae4ddbd7810be5004ff1c2f95
mjoniec <mart1n.joniec@gmail.com>
2022-02-24 15:33:56 +0100

    remove references to some local test projects

--- SearchApi/Program.cs
+++ SearchApi/Program.cs
@@ -21,6 +21,7 @@ if (app.Environment.IsDevelopment()) //??#4 good practices in enabling on produc
 
 //app.UseHttpsRedirection(); // ??#5 read on enabling https with core 6, diff in launchSettings.json comparison
 //app.UseAuthorization(); // ??#6 if not needed & no https why added by default?
+
 app.MapControllers();//??#7 - read on web application in core 6 + pipeline changes, how does MapControllers() know what folder and classes to map to?
 app.Run();//??#7
 
