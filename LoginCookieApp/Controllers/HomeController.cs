77f2d13399a3b0b8d002edc80dd4e83ad422bfa6
mjoniec <mart1n.joniec@gmail.com>
2022-03-02 23:12:02 +0100

    login with cookie ticket

--- LoginCookieApp/Controllers/HomeController.cs
+++ LoginCookieApp/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using LoginCookieApp.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace LoginCookieApp.Controllers
 {
@@ -18,11 +22,52 @@ namespace LoginCookieApp.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult ForLoggedUsers()
         {
             return View();
         }
 
+        [HttpGet("login")]//not home/login, just login for simplicity
+        public IActionResult Login(string returnUrl)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost("login")]//has to match action in view
+        public async Task<IActionResult> LoginSubmit(string user, string password, string returnUrl)
+        {
+            if(user == "test" && password == "test")
+            {
+                await HttpContext.SignInAsync(GetClaimsPrincipal(user)); 
+
+                return Redirect(returnUrl);
+            }
+
+            return BadRequest("login submit unsuccessful");
+        }
+
+        //this is needed for app to know thet the user is verified
+        //after positive verification in order for
+        //return redirection to secure page to work
+        //else it will loop again into login page
+        private ClaimsPrincipal GetClaimsPrincipal(string user)
+        {
+            var claims = new List<Claim>
+                {
+                    new Claim("user", user),
+                    new Claim(ClaimTypes.NameIdentifier, user)
+                };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            //authentication ticket
+            //to hand each time authentication is needed
+            //instead of passing password validation all over again
+            //stored in cookies
+            return new ClaimsPrincipal(claimsIdentity);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- LoginCookieApp/Program.cs
+++ LoginCookieApp/Program.cs
@@ -1,23 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(o =>
+    {
+        o.LoginPath = "/login";//this will redirect to login page after click on forLoggedUsers (if not logged)
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseStaticFiles();
+app.UseStaticFiles();//affects looks of nav bar on the page
 
 app.UseRouting();
-
-app.UseAuthorization();
+app.UseAuthentication();//cookie will get created
+app.UseAuthorization();//gives return url when clicked on link to endpoint with attribute [Authorize]
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
