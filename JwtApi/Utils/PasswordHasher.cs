bebd150474d629530f83846e6326d79df97ae3a3
mjoniec <mart1n.joniec@gmail.com>
2022-03-03 18:33:54 +0100

    move utils to jwtApi, delete utils separate project lib

--- JwtApi/Utils/PasswordHasher.cs
+++ JwtApi/Utils/PasswordHasher.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
-namespace Utils
+namespace JwtApi.Utils
 {
     /// <summary>
     /// changed to static after copying from this tutorial https://medium.com/@evandro.ggomes/json-web-token-authentication-with-asp-net-core-2-0-b074b0cfc870
@@ -17,29 +17,29 @@ namespace Utils
         {
             byte[] salt;
             byte[] buffer2;
-            
+
             if (string.IsNullOrEmpty(password))
             {
                 throw new ArgumentNullException("password");
             }
-            
+
             using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
             {
                 salt = bytes.Salt;
                 buffer2 = bytes.GetBytes(0x20);
             }
-            
+
             byte[] dst = new byte[0x31];
             Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
             Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
-            
+
             return Convert.ToBase64String(dst);
         }
 
         public static bool PasswordMatches(string providedPassword, string passwordHash)
         {
             byte[] buffer4;
-            
+
             if (passwordHash == null)
             {
                 return false;
@@ -48,9 +48,9 @@ namespace Utils
             {
                 throw new ArgumentNullException("providedPassword");
             }
-            
+
             byte[] src = Convert.FromBase64String(passwordHash);
-            
+
             if ((src.Length != 0x31) || (src[0] != 0))
             {
                 return false;
@@ -60,12 +60,12 @@ namespace Utils
             Buffer.BlockCopy(src, 1, dst, 0, 0x10);
             byte[] buffer3 = new byte[0x20];
             Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
-            
+
             using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(providedPassword, dst, 0x3e8))
             {
                 buffer4 = bytes.GetBytes(0x20);
             }
-            
+
             return ByteArraysEqual(buffer3, buffer4);
         }
 
@@ -83,7 +83,7 @@ namespace Utils
             }
 
             bool areSame = true;
-            
+
             for (int i = 0; i < a.Length; i++)
             {
                 areSame &= (a[i] == b[i]);
